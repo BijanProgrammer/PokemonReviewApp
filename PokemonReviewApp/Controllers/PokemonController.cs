332a7b77c236637ae9bf6f06b2e4568ebb7752c2
BijanProgrammer <bijaneisapour@gmail.com>
2023-04-23 09:43:12 +0330

    [ADD] AutoMapper

--- PokemonReviewApp/Controllers/PokemonController.cs
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using PokemonReviewApp.Dto;
 using PokemonReviewApp.Interfaces;
 using PokemonReviewApp.Models;
 
@@ -10,21 +12,72 @@ namespace PokemonReviewApp.Controllers;
 public class PokemonController : Controller
 {
     private readonly IPokemonRepository _pokemonRepository;
+    private readonly IMapper _mapper;
 
-    public PokemonController(IPokemonRepository pokemonRepository)
+    public PokemonController(IPokemonRepository pokemonRepository, IMapper mapper)
     {
         _pokemonRepository = pokemonRepository;
+        _mapper = mapper;
     }
 
     [HttpGet]
-    [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<PokemonDto>))]
     public IActionResult GetPokemons()
     {
-        var pokemons = _pokemonRepository.GetPokemons();
+        var pokemons = _mapper.Map<List<PokemonDto>>(_pokemonRepository.GetPokemons());
 
         if (!ModelState.IsValid)
             return BadRequest(ModelState);
 
         return Ok(pokemons);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(200, Type = typeof(PokemonDto))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemon(int id = 2)
+    {
+        if (!_pokemonRepository.DoesPokemonExist(id))
+        {
+            return NotFound();
+        }
+
+        var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(id));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(pokemon);
+    }
+
+    [HttpGet("{name}")]
+    [ProducesResponseType(200, Type = typeof(PokemonDto))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemon(string name = "Squirtle")
+    {
+        var pokemon = _mapper.Map<PokemonDto>(_pokemonRepository.GetPokemon(name));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(pokemon);
+    }
+
+    [HttpGet("{id:int}/rating")]
+    [ProducesResponseType(200, Type = typeof(decimal))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemonRating(int id = 2)
+    {
+        if (!_pokemonRepository.DoesPokemonExist(id))
+        {
+            return NotFound();
+        }
+
+        var rating = _pokemonRepository.GetPokemonRating(id);
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(rating);
     }
 }
