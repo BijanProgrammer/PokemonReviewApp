565afb8e080954bd0031eef728c86a85dd5f0ee4
BijanProgrammer <bijaneisapour@gmail.com>
2023-04-23 09:57:08 +0330

    [ADD] category controller

---
+++ PokemonReviewApp/Controllers/CategoryController.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+
+namespace CategoryReviewApp.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CategoryController : Controller
+{
+    private readonly ICategoryRepository _categoryRepository;
+    private readonly IMapper _mapper;
+
+    public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+    {
+        _categoryRepository = categoryRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<CategoryDto>))]
+    public IActionResult GetCategories()
+    {
+        var categories = _mapper.Map<List<CategoryDto>>(_categoryRepository.GetCategories());
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(categories);
+    }
+
+    [HttpGet("{id:int}")]
+    [ProducesResponseType(200, Type = typeof(CategoryDto))]
+    [ProducesResponseType(400)]
+    public IActionResult GetCategory(int id = 2)
+    {
+        if (!_categoryRepository.DoesCategoryExist(id))
+        {
+            return NotFound();
+        }
+
+        var category = _mapper.Map<CategoryDto>(_categoryRepository.GetCategory(id));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(category);
+    }
+
+    [HttpGet("{id:int}/pokemons")]
+    [ProducesResponseType(200, Type = typeof(IEnumerable<PokemonDto>))]
+    [ProducesResponseType(400)]
+    public IActionResult GetPokemonsByCategory(int id = 2)
+    {
+        if (!_categoryRepository.DoesCategoryExist(id))
+        {
+            return NotFound();
+        }
+
+        var pokemons = _mapper.Map<List<PokemonDto>>(_categoryRepository.GetPokemonsByCategory(id));
+
+        if (!ModelState.IsValid)
+            return BadRequest(ModelState);
+
+        return Ok(pokemons);
+    }
+}
---
+++ PokemonReviewApp/Repository/CategoryRepository.cs
@@ -0,0 +1,36 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repository;
+
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly DataContext _context;
+
+    public CategoryRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<Category> GetCategories()
+    {
+        return _context.Categories.OrderBy(c => c.Id).ToList();
+    }
+
+    public Category GetCategory(int id)
+    {
+        return _context.Categories.FirstOrDefault(c => c.Id == id);
+    }
+
+    public ICollection<Pokemon> GetPokemonsByCategory(int id)
+    {
+        var pokemonCategories = _context.PokemonCategories.Where(pc => pc.CategoryId == id);
+        return pokemonCategories.Select(pc => pc.Pokemon).ToList();
+    }
+
+    public bool DoesCategoryExist(int id)
+    {
+        return _context.Categories.Any(c => c.Id == id);
+    }
+}
